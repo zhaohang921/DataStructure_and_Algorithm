49a65f4fe6e067454a4f8dbf096b1959d309421d
zhaohang <zhaohang19950921@gmail.com>
2018-03-12 16:22:48 +0800

    添加老项目

---
+++ 各种算法问题/查找/查找/test.cpp
@@ -0,0 +1,34 @@
+//#include<iostream>
+//#include<vector>
+//#include<string>
+//
+//using namespace std;
+//
+////二分搜索
+//int main()
+//{
+//	int sum = 10000;//总数
+//	int num = 999;
+//	vector<int> vec;
+//	for (int i = 0; i <= sum; i++)
+//	{
+//		vec.push_back(i);
+//	}
+//	//for (int i = 0; i < num; i++)
+//	//{
+//	//	cout<<vec[i]<<endl;
+//	//}
+//	//如果总数是奇数
+//	if ((sum + 1) % 2 == 1)
+//	{
+//
+//	}
+//	if(num<=sum/2)
+//
+//
+//
+//
+//	
+//	cin.get();//system("pause");
+//	return 0;
+//}
